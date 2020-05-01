476c57fa4ca76e44267142bfa3c9f04e50ad2537
koneru123 <koneru.priyanka@gmail.com>
2020-05-01 11:09:58 -0700

    /images route to get all the images info is working

--- schema.sql
+++ schema.sql
@@ -17,28 +17,43 @@
    userName VARCHAR(50),
    userReviews VARCHAR(255),
    userType VARCHAR(50),
-   userIconImage BLOB
+   userIconImage VARCHAR(255)
  );
 
  CREATE TABLE images_info(
    imageId int AUTO_INCREMENT NOT NULL PRIMARY KEY,
    imageTitle VARCHAR(50),
-   itemImage BLOB,
+   itemImage VARCHAR(255),
    imageCategory VARCHAR(100),
    imageDescription VARCHAR(255),
-   imageUploadDate DATE
+   imageUploadDate DATE,
+   userId int,
+   restaurantId int,
+   FOREIGN KEY (userId) REFERENCES users_info(userId),
+   FOREIGN KEY (restaurantId) REFERENCES restaurants_info(restaurantId)
  );
 
 INSERT INTO restaurants_info(restaurantName) VALUES ("Bombay Garden");
 INSERT INTO restaurants_info(restaurantName) VALUES ("India Kitchen");
+INSERT INTO restaurants_info(restaurantName) VALUES ("Vietnamese Cuisine");
 INSERT INTO restaurants_info(restaurantName) VALUES ("Japenese Cuisine");
+INSERT INTO restaurants_info(restaurantName) VALUES ("Kashmiri Cuisine");
+INSERT INTO restaurants_info(restaurantName) VALUES ("Nepalese Cuisine");
+INSERT INTO restaurants_info(restaurantName) VALUES ("Korean Cuisine");
+INSERT INTO restaurants_info(restaurantName) VALUES ("American Cuisine");
+INSERT INTO restaurants_info(restaurantName) VALUES ("Chinese Cuisine");
+
 
 INSERT INTO users_info(userName, userReviews, userType, userIconImage) 
-    VALUES ("priya", "this is an awesome place", "regular", LOAD_FILE("/Users/priyankakoneru/HR/yelp_images/images/icons/user1.jpg"));
+    VALUES ("priya", "this is an awesome place", "regular", "/Users/priyankakoneru/HR/yelp_images/images/icons/user1.jpg");
 INSERT INTO users_info(userName, userReviews, userType, userIconImage) 
-    VALUES ("koneru", "this is an amazing place", "elite", LOAD_FILE("/Users/priyankakoneru/HR/yelp_images/images/icons/user2.jpg"));
+    VALUES ("koneru", "this is an amazing place", "elite", "/Users/priyankakoneru/HR/yelp_images/images/icons/user2.jpg");
+INSERT INTO users_info(userName, userReviews, userType, userIconImage) 
+    VALUES ("hello", "this is a good place", "elite", "/Users/priyankakoneru/HR/yelp_images/images/icons/user3.jpg");
 
-INSERT INTO images_info(imageTitle, itemImage, imageCategory, imageDescription, imageUploadDate) 
-      VALUES ("testImage", "food", LOAD_FILE("/Users/priyankakoneru/HR/yelp_images/images/food1.jpg"), "yummy dish", CURDATE());
-INSERT INTO images_info(imageTitle, itemImage, imageCategory, imageDescription, imageUploadDate) 
-      VALUES ("testImage1", "decor", LOAD_FILE("/Users/priyankakoneru/HR/yelp_images/images/food2.jpg"), "nice environment", CURDATE());
+INSERT INTO images_info(imageTitle, itemImage, imageCategory, imageDescription, imageUploadDate, userId, restaurantId) 
+      VALUES ("testImage", "/Users/priyankakoneru/HR/yelp_images/images/food1.jpg", "food", "yummy dish", CURDATE(), 1, 2);
+INSERT INTO images_info(imageTitle, itemImage, imageCategory, imageDescription, imageUploadDate, userId, restaurantId) 
+      VALUES ("testImage1", "/Users/priyankakoneru/HR/yelp_images/images/food2.jpg", "decor", "nice environment", CURDATE(), 1, 2);
+INSERT INTO images_info(imageTitle, itemImage, imageCategory, imageDescription, imageUploadDate, userId, restaurantId) 
+      VALUES ("testImage2", "/Users/priyankakoneru/HR/yelp_images/images/food3.jpg", "food", "super good", CURDATE(), 1, 2);
---
+++ server/controller.js
@@ -0,0 +1,10 @@
+const model = require('../database/model.js');
+
+module.exports = {
+    getImages: (req, res) => {
+        //res.send('GET Images route working');
+        model.findAllImages(res);
+      },
+}
+
+
