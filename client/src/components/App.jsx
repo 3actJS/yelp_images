f0c478cbd948bd788884d315a33c28d4aa77cf14
koneru123 <koneru.priyanka@gmail.com>
2020-05-04 12:40:22 -0700

    Updating view to render data coming from controller instead of using sample data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import ImageContainer from './ImageContainer.jsx'
 import images from '../sampleResponse.js'
+import axios from 'axios'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            images: images
+            images: []
         }
+        this.getImages = this.getImages.bind(this);
+    }
+
+    componentDidMount() {
+        this.getImages();
+    }
+
+    // error handling
+    handleError(error) {
+        console.log(error);
+    }
+
+    getImages() {
+        axios.get('/images')
+        .then(res => this.setState({images: res.data}))
+        .catch(this.handleError) 
     }
 
     render() {
