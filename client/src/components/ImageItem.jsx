fa3cf48bee539fb1bb77d892a4bdce930dacd8b4
koneru123 <koneru.priyanka@gmail.com>
2020-05-05 21:52:14 -0700

    Image Slider works

--- client/src/components/ImageItem.jsx
+++ client/src/components/ImageItem.jsx
@@ -3,12 +3,18 @@ import styled from 'styled-components';
 
 const Div = styled.div`
   border-right: 2px solid white;
+  img {
+    height: 100%;
+    width: 350px;
+    object-fit: cover;
+    vertical-align: middle;
+  }
 `;
 
 const ImageItem = ({image}) => (
     <Div>
         <span>       
-            <img src={image.itemImageUrl} alt="food image" height="250" width="250"/>           
+            <img src={image.itemImageUrl} alt="food image"/>           
         </span>
     </Div>
 )
