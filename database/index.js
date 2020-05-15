7af89cc001356be98502189c361f7ee7936fba93
koneru123 <koneru.priyanka@gmail.com>
2020-05-15 15:23:27 -0700

    push changes

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : '54.177.12.67',
+  host     : '172.17.0.3',
   user     : 'root',
   password : 'test123',
   database : 'restaurants'
@@ -10,4 +10,3 @@ var connection = mysql.createConnection({
 connection.connect(() => console.log('connected to the database!'));
 
 module.exports = connection;
-
